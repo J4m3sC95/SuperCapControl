78706f59d14f094c401ce1dffe9b877e72cbcb9b
James <jcoulson95@gmail.com>
2018-05-29 20:26:33 +0100

    Initial commit of code

---
+++ SuperCapControl/main.cpp
@@ -0,0 +1,18 @@
+/*
+ * SuperCapControl.cpp
+ *
+ * Created: 29/05/2018 19:41:06
+ * Author : James
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
